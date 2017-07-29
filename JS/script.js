352bcace419237e6ce452de8e1ef680b5f65e647
Talha Iqbal Syed <talhaiqbal11@gmail.com>
2017-07-30 03:42:36 +0500

    4th Commit
    Glitchy smooth scroll but a smoothscroll nonetheless. New layout. Still figuring out the basic structure. Might re-do this again.

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
-  $(".description").hide();
-  
+  $(function() {
+  $('a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('.container, .scrollable').animate({
+          scrollTop: $('.scrollable').scrollTop() + target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 });
